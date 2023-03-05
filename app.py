d6c995ed8f240bc115e66681116eff7efc0b4bbd
HakusaiTh <104154862+HakusaiTH@users.noreply.github.com>
2023-03-05 20:08:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def process():
     else :
         sid = SentimentIntensityAnalyzer()
 
-        openai.api_key = "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"
+        openai.api_key = "openai.api_key"
 
         model_engine = "text-davinci-003"
 
